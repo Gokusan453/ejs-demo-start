69623233bf9ad74844183bab73a3796dee7304e4
Gokusan453 <gokhangunerr4@gmail.com>
2025-02-19 13:43:44 +0100

    ejs commit

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
 // import statements
 import express from "express";
+import expressLayouts from "express-ejs-layouts";
 import path from "path";
+import { home, about, contact, privacy } from "./controllers/PageController.js";
 
-// create an instance of express
 const app = express();
 
+// use express layouts
+app.use(expressLayouts);
 // set the view engine to ejs
 app.set("view engine", "ejs");
+app.set("layout", "layouts/main");
 app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
-// GET route to serve the index.html file
-app.get("/", (req, res) => {
-  res.render("home", {
-    title: "Dinosaurs are awesome!",
-    content: "Dinosaurs are a cool creatures",
-  })
+// page routes
+app.get("/", home);
+app.get("/about", about);
+app.get("/contact", contact);
+app.get("/privacy", privacy);
+
+// 404 page
+app.get("*", (req, res) => {
+  res.status(404).render("errors/404", {
+    layout: "layouts/error", // override the default layout
+  });
 });
 
 // start the server, listen on port defined in .env file
---
+++ src/controllers/PageController.js
@@ -0,0 +1,39 @@
+
+
+import { person, navItems } from "../data/data.js";
+
+export const home = (req, res) => {
+    res.render("pages/home", {
+        title: "Dinosaurs around the world",
+        content: "welcome to the world of dinosaurs",
+        navItems,
+    });
+};
+
+export const about = (req, res) => {
+  res.render("pages/default", {
+    title: "About us",
+    content: "We are a group of dinosaur enthusiasts",
+    team: [ "T-Rex", "Velociraptor", "Stegosaurus", "<strong>Premium</strong> Diplodocus",
+    ],
+    navItems,
+  });
+};
+
+export const contact = (req, res) => {
+  res.render("pages/contact", {
+    title: "Contact",
+    content: "Contact us at 09 233 40 89",
+    person,
+    navItems,
+  });
+};
+
+export const privacy = (req, res) => {
+  res.render("pages/default", {
+    title: "Privacy Policy",
+    content: "Dinosaurs are protective of their privacy",
+    team: false,
+    navItems,
+  });
+};
